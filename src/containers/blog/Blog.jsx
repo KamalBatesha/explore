1354fb0b16a879090588db336e8cea5767aa5feb
KamalBatesha <bateshakamal@gmail.com>
2024-02-12 22:13:46 +0200

    add blog section

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./blog.css";
+import { Article } from "../../components";
+import data from "./imports";
 
 function Blog() {
-  return <div>Blog </div>;
+  return (
+    <div className="gpt3__blog section__padding" id="blog">
+      <div className="gpt3__blog-headding">
+        <h1 className="gradient__text">
+          A lot is happening, We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt3__blog-container">
+        {data.map((item) => (
+          <Article
+            date={item.date}
+            title={item.title}
+            imgurl={item.image}
+            key={item.id}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Blog;
---
+++ src/containers/blog/imports.js
@@ -0,0 +1,40 @@
+import blog01 from "../../assets/blog01.png";
+import blog02 from "../../assets/blog02.png";
+import blog03 from "../../assets/blog03.png";
+import blog04 from "../../assets/blog04.png";
+import blog05 from "../../assets/blog05.png";
+
+const data = [
+  {
+    id: 1,
+    image: blog01,
+    title: "GPT-3 and Open  AI is the future. Let us exlore how it is?",
+    date: "Sep 26, 2021",
+  },
+  {
+    id: 2,
+    image: blog02,
+    title: "GPT-3 and Open  AI is the future. Let us exlore how it is?",
+    date: "Sep 26, 2021",
+  },
+  {
+    id: 3,
+    image: blog03,
+    title: "GPT-3 and Open  AI is the future. Let us exlore how it is?",
+    date: "Sep 26, 2021",
+  },
+  {
+    id: 4,
+    image: blog04,
+    title: "GPT-3 and Open  AI is the future. Let us exlore how it is?",
+    date: "Sep 26, 2021",
+  },
+  {
+    id: 5,
+    image: blog05,
+    title: "GPT-3 and Open  AI is the future. Let us exlore how it is?",
+    date: "Sep 26, 2021",
+  },
+];
+
+export default data;
